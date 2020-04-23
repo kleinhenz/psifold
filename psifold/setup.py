143b8abf83ac137296a733d6f2351ad285e4c0ad
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2020-04-22 20:27:25 -0400

    add tqdm to setup.py

--- psifold/setup.py
+++ psifold/setup.py
@@ -3,7 +3,7 @@ setup(
     name="psifold",
     version="0.1",
     packages=find_packages(),
-    install_requires=["numpy", "h5py", "matplotlib", "torch"],
+    install_requires=["numpy", "h5py", "matplotlib", "torch", "tqdm"],
     python_requires="~=3.6",
     entry_points = {
         "console_scripts" : ["psifold_train=psifold.scripts.psifold_train:main",

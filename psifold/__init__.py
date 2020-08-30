4c7f7e2e01442856bed40a6adad0a63fc4a6e11a
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2020-08-29 17:46:14 -0700

    add LAMB optimizer and util for lr scheduling

--- psifold/__init__.py
+++ psifold/__init__.py
@@ -1,6 +1,7 @@
 from .geometry import dRMSD, dRMSD_masked, internal_coords, internal_to_srf, nerf, pnerf
 from .data import make_data_loader
 from .util import count_parameters, to_device, group_by_class
+from .optimization import Lamb, poly_schedule
 
 from . import models
 from . import scripts

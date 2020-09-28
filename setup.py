0e1f0b377d5fb2c5b273ce3ba54cfd391f47a205
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2020-09-27 17:30:29 -0700

    require pytorch 1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name="psifold",
     version="0.1",
     packages=find_packages(),
-    install_requires=["numpy", "h5py", "matplotlib", "torch>=1.5", "tqdm", "tensorflow", "pytest"],
+    install_requires=["numpy", "h5py", "matplotlib", "torch>=1.6", "tqdm", "tensorflow", "pytest"],
     python_requires="~=3.6",
     entry_points = {
         "console_scripts" : ["run_rgn=psifold.scripts.run_rgn:main",
